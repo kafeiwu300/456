0a6b3a065f96a04d1a140247d69224dfa658f11a
zhaotong <2248620869@qq.com>
2019-11-20 11:18:22 +0800

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,10 @@ const App: React.FC = () => {
     <Provider store={store}>
       <Router history={createBrowserHistory()}>
         <Switch>
-<<<<<<< HEAD
           <Route path="/:projectId/:iterationId/story-map" component={StoryMap}/>
           <Route path="/:projectId/kanban" component={Kanban}/>
-          <Route path="/:projectId" component={Home}/>
-=======
-          <Route path="/story-map" component={StoryMap}/>
-          <Route path="/kanban" component={Kanban}/>
           <Route path="/bug" component={Bug}/>
-          <Route path="/" component={Home}/>
->>>>>>> d7d9ba5acec1a1ea1a35a4fb6d2e4a1caf0bcba0
+          <Route path="/:projectId" component={Home}/>
         </Switch>
       </Router>
     </Provider>
