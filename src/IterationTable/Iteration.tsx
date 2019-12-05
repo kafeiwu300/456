e35cce8bd12742b536c8bb40b7d53941ec8c97db
zhaotong <2248620869@qq.com>
2019-12-05 11:30:31 +0800

    fix:修改按钮样式

--- src/IterationTable/Iteration.tsx
+++ src/IterationTable/Iteration.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Layout, PageHeader, Switch } from "antd";
+import { Layout, PageHeader, Radio } from "antd";
 import Kanban from "../Kanban/Kanban";
 import { IIterationInfo } from "../StoryMap/interfaces";
 import { connect } from "react-redux";
@@ -9,7 +9,7 @@ import BurnDown from "../BurnDown/BurnDown";
 import { store } from "../store";
 
 const Iteration: React.FC<{iteration?: IIterationInfo}> = ({iteration}) => {
-  const [showKanban, setShowKanban] = useState<boolean>(true);
+  const [content, setContent] = useState<string>('kanban');
   
   const { match, history } = useRouter<{
     projectId: string,
@@ -27,9 +27,14 @@ const Iteration: React.FC<{iteration?: IIterationInfo}> = ({iteration}) => {
 
   return (
     <Layout>
-      <PageHeader title={iteration ? iteration.title : '迭代'} onBack={() => {history.goBack()}} extra={<Switch checkedChildren='看板' unCheckedChildren='燃尽图' defaultChecked onChange={() => setShowKanban(!showKanban)}/>}/>
+      <PageHeader title={iteration ? iteration.title : '迭代'} onBack={() => {history.goBack()}} extra={
+        <Radio.Group buttonStyle="solid" defaultValue={content} onChange={(e) => setContent(e.target.value)}>
+          <Radio.Button value='kanban'>看板</Radio.Button>
+          <Radio.Button value='burn-down'>燃尽图</Radio.Button>
+        </Radio.Group>
+      }/>
       <Layout.Content style={{overflowX: 'scroll'}}>
-        {showKanban ? <Kanban/> : <BurnDown/>}
+        {content === 'kanban' ? <Kanban/> : <BurnDown/>}
       </Layout.Content>
     </Layout>
   );
