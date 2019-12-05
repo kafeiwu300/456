608da6a31a0c8699f7c609f92f9d96850ca1ba1b
zhaotong <2248620869@qq.com>
2019-12-05 11:24:57 +0800

    fix:迭代的开始、结束时间格式化显示

--- src/IterationTable/IterationTable.tsx
+++ src/IterationTable/IterationTable.tsx
@@ -6,6 +6,7 @@ import { Table } from "antd";
 import Column from "antd/lib/table/Column";
 import useRouter from "use-react-router";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 const IterationTable: React.FC<{iterations: IIterationInfo[]}> = ({iterations}) => {
   const { match } = useRouter<{
@@ -17,11 +18,11 @@ const IterationTable: React.FC<{iterations: IIterationInfo[]}> = ({iterations}) 
   return (
     <>
       <Table dataSource={iterations} style={{backgroundColor: '#fff'}}>
-        <Column title='标题' dataIndex='title' key='title' render={(text: any, record: IIterationInfo) => <Link to={`/project/${projectId}/iteration/${record.id}`}>{record.title}</Link>}/>
+        <Column title='标题' dataIndex='title' key='title' render={(text: string, record: IIterationInfo) => <Link to={`/project/${projectId}/iteration/${record.id}`}>{record.title}</Link>}/>
         <Column title='迭代目标' dataIndex='target' key='target'/>
         <Column title='负责人' dataIndex='leader' key='leader'/>
-        <Column title='开始时间' dataIndex='startTime' key='startTime'/>
-        <Column title='结束时间' dataIndex='endTime' key='endTime'/>
+        <Column title='开始时间' dataIndex='startTime' key='startTime' render={(text: string) => moment(text).format('YYYY-MM-DD HH:mm:ss')}/>
+        <Column title='结束时间' dataIndex='endTime' key='endTime' render={(text: string) => moment(text).format('YYYY-MM-DD HH:mm:ss')}/>
       </Table>
     </>
   );
--- src/common/consts.ts
+++ src/common/consts.ts
@@ -3,6 +3,6 @@ export const PRIMARY_COLOR = '#13c2c2';
 export const DEBUG = true;
 
 // export const BASE_URL = process.env.REACT_APP_HOST;
-export const BASE_URL = 'http://192.168.1.101:11021';
+export const BASE_URL = 'http://192.168.1.106:11021';
 
 export const AGILE_BASE_URL = `${BASE_URL}/api/agile`;
