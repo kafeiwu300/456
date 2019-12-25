7b3c0ea839d7b55c19e6e0387ab20343ff74af60
zhaotong <2248620869@qq.com>
2019-12-25 10:53:56 +0800

    fix:修复修改用例后详情不更新的bug

--- src/Test/TestCaseList.tsx
+++ src/Test/TestCaseList.tsx
@@ -13,7 +13,7 @@ const TestCaseList: React.FC = () => {
   const { projectId } = match.params;
 
   const [testCases, setTestCases] = useState<ITestCase[]>([]);
-  const [selectedCase, setSelectedCase] = useState<ITestCase>();
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
   
   useEffect(() => {
     // getProjectStories(projectId).then(res => setStories(res.body));
@@ -81,51 +81,26 @@ const TestCaseList: React.FC = () => {
     <Layout style={{height: '100%'}}>
       <Layout.Sider theme='light' style={{height: '100%', overflow: 'auto'}}>
         <Menu>
-          {/* {
-            stories.map((story: IStoryInfo) => (
-              <Menu.SubMenu title={story.title}>
-                {
-                  testCases.filter((c: ITestCase) => )
-                }
-              </Menu.SubMenu>
-            ))
-          } */}
           {
-            testCases.map((c: ITestCase) => (
-              <Menu.Item onClick={() => setSelectedCase(c)}>
+            testCases.map((c: ITestCase, index: number) => (
+              <Menu.Item onClick={() => setSelectedIndex(index)}>
                 <span>{c.title}</span>
                 <span style={{float: 'right'}}>
-                  {modifyCase ? <Icon type='edit' onClick={() => {
+                  <Icon type='edit' onClick={() => {
                     modifyCase(c);
-                  }}/> : <></>}
-                  {deleteCase ? <Icon type='delete' onClick={() => {
+                  }}/>
+                  <Icon type='delete' onClick={() => {
                     deleteCase(c.id!);
-                  }}/> : <></>}
+                  }}/>
                 </span>
               </Menu.Item>
             ))
           }
           {newCase ? <Menu.Item onClick={newCase}><Icon type='plus'/>新建用例</Menu.Item> : <></>}
         </Menu>
-        {/* <Tree.DirectoryTree onSelect={() => {}}>
-          <Tree.TreeNode title="用例包">
-            {
-              data.map((story: IStoryWithCase) => (
-                <Tree.TreeNode title={story.title}>
-                  {
-                    story.testCases.map((testCase: ITestCase) => <Tree.TreeNode title={testCase.title} isLeaf={true}/>)
-                  }
-                </Tree.TreeNode>
-              ))
-            }
-            {
-              testCases.map((c: ITestCase, index: number) => <Tree.TreeNode key={String(index)} title={c.title} isLeaf={true}/>)
-            }
-          </Tree.TreeNode>
-        </Tree.DirectoryTree> */}
       </Layout.Sider>
       <Layout.Content>
-        <TestCaseInfo testCase={selectedCase}/>
+        <TestCaseInfo testCase={selectedIndex >= 0 && selectedIndex < testCases.length ? testCases[selectedIndex] : undefined}/>
       </Layout.Content>
     </Layout>
   );
