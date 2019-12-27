87aab4aa9b8f43981374153721540b675b067685
zhaotong <2248620869@qq.com>
2019-12-27 14:59:51 +0800

    feat(header): 点击左上角后跳转回首页

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import Home from './Home';
-import Project from './Project';
-import { Layout, Typography } from 'antd';
+import React from "react";
+import "./App.css";
+import { Router, Route, Switch, Link } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Home from "./Home";
+import Project from "./Project";
+import { Layout, Typography } from "antd";
 
 const App: React.FC = () => {
   return (
     <Router history={createBrowserHistory()}>
-      <Layout style={{height: '100%', width: '100%'}}>
-        <Layout.Header style={{ background: '#fff' }}>
-          <Typography.Title level={3}>敏捷实践系统</Typography.Title>
+      <Layout style={{ height: "100%", width: "100%" }}>
+        <Layout.Header style={{ background: "#fff" }}>
+          <Link to="/">
+            <Typography.Title level={3}>敏捷实践系统</Typography.Title>
+          </Link>
         </Layout.Header>
-        <Layout.Content style={{height: 'calc(100% - 64px)'}}>
+        <Layout.Content style={{ height: "calc(100% - 64px)" }}>
           <Switch>
-            <Route path="/project/:projectId" component={Project}/>
-            <Route path="/" component={Home}/>
+            <Route path="/project/:projectId" component={Project} />
+            <Route path="/" component={Home} />
           </Switch>
         </Layout.Content>
       </Layout>
     </Router>
   );
-}
+};
 
 export default App;
--- src/LayoutRoute.tsx
+++ src/LayoutRoute.tsx
@@ -6,23 +6,26 @@ interface Props extends RouteProps {
   title: React.ReactNode;
 }
 
-const LayoutRoute: React.FC<Props> = ({title, component, ...rest}) => {
+const LayoutRoute: React.FC<Props> = ({ title, component, ...rest }) => {
   const Component = component as React.ReactType;
 
   return (
-    <>
-      <Route render={() => (
-        <Layout style={{height: '100%'}}>
-          <PageHeader title={title}/>
-          <Layout style={{height: '100%'}}>
-            <Layout.Content style={{height: '100%', margin: '0 24px', overflow: 'auto'}}>
-              <Component/>
+    <Route
+      render={() => (
+        <Layout style={{ height: "100%" }}>
+          <PageHeader title={title} />
+          <Layout style={{ height: "100%" }}>
+            <Layout.Content
+              style={{ height: "100%", margin: "0 24px", overflow: "auto" }}
+            >
+              <Component />
             </Layout.Content>
           </Layout>
         </Layout>
-      )} {...rest}/>
-    </>
+      )}
+      {...rest}
+    />
   );
-}
+};
 
 export default LayoutRoute;
